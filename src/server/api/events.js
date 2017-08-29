f009a11e72d76ca16f5e012570e37f5de3f8c12e
John <john@funkhaus.us>
2017-08-29 15:32:19 -0400

    Added events endpoint 	- added responses utility to shared app folder

---
+++ src/server/api/events.js
@@ -0,0 +1,27 @@
+import { succeed } from 'app/responses'
+import knex from 'app/db'
+import _ from 'lodash'
+
+const handler = async (req, res, next) => {
+
+    let orderBy = req.query.orderBy || 'date',
+        order = req.query.order || 'DESC',
+        limit = req.query.limit ? Math.min(req.query.limit, 100) : 25,
+        offset = req.query.offset || 0
+
+    const getEvents = () => knex('events')
+
+    const [data, count] = await Promise.all([
+        getEvents().offset(offset).orderBy(orderBy, order).limit(limit),
+        getEvents().count().then(_.first)
+    ])
+    const pagination = {
+        total: parseInt(count.count) || 0,
+        limit,
+        offset
+    }
+
+    return succeed({ res, msg: 'Successfully retrieved events.', add: { pagination, data } })
+}
+
+export default handler
--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,7 +1,10 @@
+import eventsRoute from './events'
 import { Router } from 'express'
 
 const router = new Router({ mergeParams: true })
 
+// heartbeat
+router.use('/events', eventsRoute)
 router.use('/', (req, res) => res.send('API is up and running.'))
 
 export default router
